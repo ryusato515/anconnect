c37e6044162a05f97e7b5458b337927e29d044d7
satoryu <ryu@satoryunoMacBook-Air.local>
2024-03-15 17:21:05 +0900

    “ログインとログアウト機能実装完了”

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  authenticates_with_sorcery!
+
+  validates :password, length: { minimum: 5 }, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*[0-9]).{5,}\z/ }, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
+  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
+  #validates :reset_password_token, uniqueness: true, allow_nil: true
+
+  validates :email, uniqueness: true, presence: true
+  validates :name, presence: true, length: { maximum: 255 }
+end
