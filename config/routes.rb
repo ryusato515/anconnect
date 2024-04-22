4dc40b35c85da406f24cf823852647684b52631a
satoryu <ryu@satoryunoMacBook-Air.local>
2024-04-22 14:32:12 +0900

    “パスワードリセット機能作成”

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   root 'static_pages#top'
 
   resources :users, only: %i[new create show update]
@@ -12,6 +13,7 @@ Rails.application.routes.draw do
 
   resource :profile, only: %i[show edit update]
   resources :profiles, only: :index
+  resources :password_resets, only: %i[new create edit update]
 
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_02_004001) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_22_014632) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -62,7 +62,12 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_02_004001) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "avatar"
+    t.string "reset_password_token"
+    t.datetime "reset_password_token_expires_at"
+    t.datetime "reset_password_email_sent_at"
+    t.integer "access_count_to_reset_password_page", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "favorites", "posts"
