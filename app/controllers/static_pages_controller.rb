5502a7336ec66a8b58261a60a706aa68e771265c
satoryu <ryu@satoryunoMacBook-Air.local>
2024-05-29 20:08:21 +0900

    “デプロイ”

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,7 @@
 class StaticPagesController < ApplicationController
-  skip_before_action :require_login, only: %i[top]
+  skip_before_action :require_login, only: %i[top privacy_policy terms_of_service]
 
   def top; end
+  def privacy_policy; end
+  def terms_of_service; end
 end
