5422410d0de1e880faa190cf278e7c61351994b7
satoryu <ryu@satoryunoMacBook-Air.local>
2024-06-14 16:27:14 +0900

    “OGP設定”

--- Gemfile
+++ Gemfile
@@ -67,6 +67,8 @@ gem 'dotenv-rails'
 
 gem 'kaminari'
 gem 'config'
+
+gem 'meta-tags'
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,31 @@ module ApplicationHelper
     else 'bg-gray-500'
     end
   end
+
+  def default_meta_tags
+    {
+      site: 'あんこねくと',
+      title: '',
+      reverse: true,
+      separator: '',
+      description: '全国各地の魅力的なあんこ菓子やスイーツの情報を集め、共有するサービスです。',
+      keywords: 'あんこ, どら焼き, 最中, 羊羹, 大福, あんこスイーツ, こしあん, つぶあん',
+      canonical: request.original_url,
+      og: {
+        title: :title,
+        description: :description,
+        type: 'website',
+        url: request.original_url,
+        image: image_url('ogp.png'),
+        site_name: ''
+      },
+      twitter: {
+        card: 'summary_large_image',
+        site: '@',
+        title: 'あんこねくと',
+        description: :description,
+        image: image_url('ogp.png')
+      }
+    }
+  end
 end
