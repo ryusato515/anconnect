2bf8f2f0fa7d1a7f7964f21a82bb555a33b1e149
satoryu <ryu@satoryunoMacBook-Air.local>
2024-06-19 19:15:10 +0900

    “独自ドメイン設定”

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,19 +36,19 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'anconnect.fly.dev', protocol: 'https' }
+  config.action_mailer.default_url_options = { host: 'an-connect.com', protocol: 'https' }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'anconnect.fly.dev',
+    domain:               'an-connect.com',
     user_name:            ENV['GMAIL_USERNAME'],
     password:             ENV['GMAIL_PASSWORD'],
     authentication:       'plain',
     enable_starttls_auto: true
   }
 
-  config.hosts << "anconnect.fly.dev"
+  config.hosts << "an-connect.com"
 end
